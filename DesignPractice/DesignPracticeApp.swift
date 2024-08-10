a64a46e0ae4ac60759b71114cd75c25b8fcfe029
Bbeniful <bbenikeszi@gmail.com>
2024-08-10 11:15:34 +0200

    Initial Commit

---
+++ DesignPractice/DesignPracticeApp.swift
@@ -0,0 +1,32 @@
+//
+//  DesignPracticeApp.swift
+//  DesignPractice
+//
+//  Created by Benjámin Szilágyi on 10/08/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DesignPracticeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
